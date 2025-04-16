6c467c16bd98fb3b94a5a6559333e67b0fb09c19
Jayesh <jayeshthakur47@gmail.com>
2025-04-16 15:08:06 +0900

    added loader to login

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -14,12 +14,13 @@ import { Label } from '@/components/ui/label';
 import Link from 'next/link';
 import { useActionState } from 'react';
 import { login } from '@/lib/actions';
+import { Loader2 } from 'lucide-react';
 
 export function LoginForm({
     className,
     ...props
 }: React.ComponentPropsWithoutRef<'div'>) {
-    const [state, action] = useActionState(login, undefined);
+    const [state, action, isPending] = useActionState(login, undefined);
     return (
         <div className={cn('flex flex-col gap-6', className)} {...props}>
             <Card>
@@ -68,9 +69,17 @@ export function LoginForm({
                                     </p>
                                 )}
                             </div>
-                            <Button type='submit' className='w-full'>
-                                Login
-                            </Button>
+                            {!isPending && (
+                                <Button type='submit' className='w-full'>
+                                    Login
+                                </Button>
+                            )}
+                            {isPending && (
+                                <Button disabled>
+                                    <Loader2 className='animate-spin' />
+                                    Please wait
+                                </Button>
+                            )}
                         </div>
                         <div className='mt-4 text-center text-sm'>
                             Don&apos;t have an account?{' '}
