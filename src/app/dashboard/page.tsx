09f6e5b15da26a16e513d41473c9c612d044b21b
Jayesh <jayeshthakur47@gmail.com>
2025-04-18 16:47:17 +0900

    added carousel of likes on Dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,55 @@
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from '@/components/ui/carousel';
 import { Separator } from '@/components/ui/separator';
+import { getLikedAnimes } from '@/lib/anime-apis';
+import Image from 'next/image';
 
-export default function Page() {
+export default async function Page() {
+    const userLikes = await getLikedAnimes();
     return (
-        <div className='flex'>
-            <div>User Area</div>
-            <Separator
-                orientation='vertical'
-                decorative
-                style={{ margin: '0 15px' }}
-            />
-            <div>cards</div>
+        <div className='m-10 flex-col'>
+            <div className='text-bold text-4xl'>My Likes</div>
+            <Separator className='my-4' />
+            <div className='justify-items-start mx-10 w-full'>
+                <Carousel className='w-full max-w-5xl'>
+                    <CarouselContent>
+                        {userLikes?.map((e) => (
+                            <CarouselItem
+                                key={e.slug}
+                                className='md:basis-1/2 lg:basis-1/3'
+                            >
+                                <Card className='m-3'>
+                                    <CardContent className='justify-items-center'>
+                                        <Image
+                                            src={e.images.originalPosterImage}
+                                            alt={e.slug}
+                                            width={200}
+                                            height={300}
+                                        />
+                                    </CardContent>
+                                    <CardFooter className='justify-center'>
+                                        <CardTitle>{e.name}</CardTitle>
+                                    </CardFooter>
+                                </Card>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
+            </div>
         </div>
     );
 }
