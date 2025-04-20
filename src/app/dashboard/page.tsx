6ecd91370a9999d6b7b667b2e5034f09065ff689
Jayesh <jayeshthakur47@gmail.com>
2025-04-20 15:09:14 +0900

    added loading skeleton to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,5 @@
-import {
-    Card,
-    CardContent,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from '@/components/ui/card';
+'use client';
+import { Card, CardContent, CardFooter, CardTitle } from '@/components/ui/card';
 import {
     Carousel,
     CarouselContent,
@@ -13,42 +8,65 @@ import {
     CarouselPrevious,
 } from '@/components/ui/carousel';
 import { Separator } from '@/components/ui/separator';
+import { Skeleton } from '@/components/ui/skeleton';
 import { getLikedAnimes } from '@/lib/anime-apis';
+import { Anime } from '@/lib/definitions';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-export default async function Page() {
-    const userLikes = await getLikedAnimes();
+export default function Page() {
+    const [userLikes, setUserLikes] = useState<[Anime] | undefined>(undefined);
+    const router = useRouter();
+    useEffect(() => {
+        const updateUserLikes = async () => {
+            setUserLikes(await getLikedAnimes());
+        };
+        updateUserLikes();
+    }, []);
     return (
         <div className='m-10 flex-col'>
             <div className='text-bold text-4xl'>My Likes</div>
             <Separator className='my-4' />
             <div className='justify-items-start mx-10 w-full'>
-                <Carousel className='w-full max-w-5xl'>
-                    <CarouselContent>
-                        {userLikes?.map((e) => (
-                            <CarouselItem
-                                key={e.slug}
-                                className='md:basis-1/2 lg:basis-1/3'
-                            >
-                                <Card className='m-3'>
-                                    <CardContent className='justify-items-center'>
-                                        <Image
-                                            src={e.images.originalPosterImage}
-                                            alt={e.slug}
-                                            width={200}
-                                            height={300}
-                                        />
-                                    </CardContent>
-                                    <CardFooter className='justify-center'>
-                                        <CardTitle>{e.name}</CardTitle>
-                                    </CardFooter>
-                                </Card>
-                            </CarouselItem>
-                        ))}
-                    </CarouselContent>
-                    <CarouselPrevious />
-                    <CarouselNext />
-                </Carousel>
+                {userLikes && (
+                    <Carousel className='w-full max-w-5xl'>
+                        <CarouselContent>
+                            {userLikes?.map((anime) => (
+                                <CarouselItem
+                                    key={anime.slug}
+                                    className='md:basis-1/2 lg:basis-1/3'
+                                >
+                                    <Card className='m-3'>
+                                        <CardContent className='justify-items-center'>
+                                            <Image
+                                                src={
+                                                    anime.images
+                                                        .originalPosterImage
+                                                }
+                                                alt={anime.slug}
+                                                width={200}
+                                                height={300}
+                                                onClick={() =>
+                                                    router.push(
+                                                        `/anime/${anime.slug}`
+                                                    )
+                                                }
+                                                className='hover:cursor-pointer'
+                                            />
+                                        </CardContent>
+                                        <CardFooter className='justify-center'>
+                                            <CardTitle>{anime.name}</CardTitle>
+                                        </CardFooter>
+                                    </Card>
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                        <CarouselPrevious />
+                        <CarouselNext />
+                    </Carousel>
+                )}
+                {!userLikes && <Skeleton className='h-64 w-5xl mx-4' />}
             </div>
         </div>
     );
