4f5834f462fe91e29a76ec49b087e4b9c2375820
Jayesh <jayeshthakur47@gmail.com>
2025-05-02 21:13:25 +0900

    made anime card component a server component

--- src/components/anime-card.tsx
+++ src/components/anime-card.tsx
@@ -1,7 +1,8 @@
-'use client';
+// 'use client';
 import { Card, CardContent, CardFooter, CardTitle } from '@/components/ui/card';
 import { Anime } from '@/lib/definitions';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 interface CardProps {
@@ -11,18 +12,20 @@ interface CardProps {
 
 export default function AnimeCard(props: CardProps) {
     const anime = props.anime;
-    const router = useRouter();
+    // const router = useRouter();
     return (
         <Card className='m-3'>
             <CardContent>
-                <Image
-                    height={500}
-                    width={300}
-                    src={anime.images.largePosterImage}
-                    alt={`poster image for ${anime.slug}`}
-                    onClick={() => router.push(`/anime/${anime.slug}`)}
-                    className='hover:cursor-pointer'
-                />
+                <Link href={`/anime/${anime.slug}`}>
+                    <Image
+                        height={500}
+                        width={300}
+                        src={anime.images.largePosterImage}
+                        alt={`poster image for ${anime.slug}`}
+                        // onClick={() => router.push(`/anime/${anime.slug}`)}
+                        className='hover:cursor-pointer'
+                    />
+                </Link>
             </CardContent>
             <CardFooter>
                 <CardTitle>
