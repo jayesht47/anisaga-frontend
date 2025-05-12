e0266cdaf1a00290b4c75128b0cf87b255c2764d
Jayesh <jayeshthakur47@gmail.com>
2025-05-12 16:13:23 +0900

    added skeletons

--- src/app/user-list/[list-name]/page.tsx
+++ src/app/user-list/[list-name]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import AnimeCard from '@/components/anime-card';
+import { Skeleton } from '@/components/ui/skeleton';
 import { getCustomList } from '@/lib/anime-apis';
 import { Anime } from '@/lib/definitions';
 import { usePathname } from 'next/navigation';
@@ -7,34 +8,41 @@ import { useEffect, useState } from 'react';
 
 export default function Page() {
     const [animeList, setAnimeList] = useState<Anime[]>();
+    const [error, setError] = useState(false);
     const currentPathName = usePathname();
     const listName = currentPathName?.split('/')[2] as string;
 
     useEffect(() => {
         const updateAnimeList = async () => {
             const response = await getCustomList(listName);
-            setAnimeList(response.data as Anime[]);
+            if (response.error === 'true') {
+                setError(true);
+            } else setAnimeList(response.data as Anime[]);
         };
         updateAnimeList();
     }, [listName]);
 
     return (
         <div className='flex-row'>
-            <h1 className='text-3xl flex justify-center mb-5'>
-                {listName}
-            </h1>
+            <h1 className='text-3xl flex justify-center mb-5'>{listName}</h1>
             <div className='flex flex-wrap justify-center'>
-                {animeList?.map((item, index) => (
-                    <AnimeCard
-                        anime={item}
-                        index={index}
-                        key={`card-${index}`}
-                    />
-                ))}
-                {animeList?.length === 0 && (
+                {!animeList && <Skeleton className='h-130 w-5xl mx-4' />}
+                {animeList &&
+                    animeList?.map((item, index) => (
+                        <AnimeCard
+                            anime={item}
+                            index={index}
+                            key={`card-${index}`}
+                            includeRemoveIcon={true}
+                        />
+                    ))}
+                {error && (
                     <p className='text-3xl text-red-400'>
                         Internal Server Error
                     </p>
+                )}
+                {animeList && animeList.length === 0 && (
+                    <p className='text-3xl text-red-400'>Empty list!</p>
                 )}
             </div>
         </div>
